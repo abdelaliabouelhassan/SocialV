49ff9d4eb2179904a9e8a3d00d4d4ceb4bc59f7a
abdelali <abdelali54abouelhassan@gmail.com>
2020-11-22 20:46:12 +0100

    test ssl

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,15 +11,15 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="shortcut icon" href="{{asset("images/favicon.ico")}}" />
+        <link rel="shortcut icon" href="images/favicon.ico" />
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="{{asset("css/bootstrap.min.css")}}">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
         <!-- Typography CSS -->
-        <link rel="stylesheet" href="{{asset("css/typography.css")}}">
+        <link rel="stylesheet" href="css/typography.css">
         <!-- Style CSS -->
-        <link rel="stylesheet" href="{{asset("css/style.css")}}">
+        <link rel="stylesheet" href="css/style.css">
         <!-- Responsive CSS -->
-        <link rel="stylesheet" href="{{asset("css/responsive.css")}}">
+        <link rel="stylesheet" href="css/responsive.css">
 {{--        <link rel="stylesheet" href="{{ asset('css/app.css') }}">--}}
 
         <!-- Scripts -->
@@ -30,35 +30,36 @@
             {{ $slot }}
         </div>
 
-        <script src="{{asset("js/jquery.min.js")}}"></script>
-        <script src="{{asset("js/popper.min.js")}}"></script>
-        <script src="{{asset("js/bootstrap.min.js")}}"></script>
+       <script src="js/jquery.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
         <!-- Appear JavaScript -->
-        <script src="{{asset("js/jquery.appear.js")}}"></script>
+        <script src="js/jquery.appear.js"></script>
         <!-- Countdown JavaScript -->
-        <script src="{{asset("js/countdown.min.js")}}"></script>
+        <script src="js/countdown.min.js"></script>
         <!-- Counterup JavaScript -->
-        <script src="{{asset("js/waypoints.min.js")}}"></script>
-        <script src="{{asset("js/jquery.counterup.min.js")}}"></script>
+        <script src="js/waypoints.min.js")}}></script>
+        <script src="js/jquery.counterup.min.js"></script>
         <!-- Wow JavaScript -->
-        <script src="{{asset("js/wow.min.js")}}"></script>
+        <script src="js/wow.min.js"></script>
         <!-- Apexcharts JavaScript -->
-        <script src="{{asset("js/apexcharts.js")}}"></script>
+        <script src="js/apexcharts.js"></script>
         <!-- lottie JavaScript -->
-        <script src="{{asset("js/lottie.js")}}"></script>
+        <script src="js/lottie.js"></script>
         <!-- Slick JavaScript -->
-        <script src="{{asset("js/slick.min.js")}}"></script>
+        <script src="js/slick.min.js"></script>
         <!-- Select2 JavaScript -->
-        <script src="{{asset("js/select2.min.js")}}"></script>
+        <script src="js/select2.min.js"></script>
         <!-- Owl Carousel JavaScript -->
-        <script src="{{asset("js/owl.carousel.min.js")}}"></script>
+        <script src="js/owl.carousel.min.js"></script>
         <!-- Magnific Popup JavaScript -->
-        <script src="{{asset("js/jquery.magnific-popup.min.js")}}"></script>
+        <script src="js/jquery.magnific-popup.min.js"></script>
         <!-- Smooth Scrollbar JavaScript -->
-        <script src="{{asset("js/smooth-scrollbar.js")}}"></script>
+        <script src="js/smooth-scrollbar.js"></script>
         <!-- Chart Custom JavaScript -->
-        <script src="{{asset("js/chart-custom.js")}}"></script>
+        <script src="js/chart-custom.js"></script>
         <!-- Custom JavaScript -->
-        <script src="{{asset("js/custom.js")}}"></script>
+        <script src="js/custom.js"></script>
+      
     </body>
 </html>
